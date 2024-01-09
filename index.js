f1187016fa426e0c498f5ffc3c126d92ecf3b5ab
akmindaugas <mindaugas.akelis@gmail.com>
2024-01-09 10:32:10 +0200

    commit_some_progress

--- index.js
+++ index.js
@@ -19,19 +19,45 @@
 // // correctName('pettras', consoleName)
 // callAlert('pettras', callAlert)
 
-function consoleName(Vardas){
-    console.log(`Vardas yra ${Vardas}`);
-}
+// function consoleName(Vardas){
+//     console.log(`Vardas yra ${Vardas}`);
+// }
 
-function callAlert(Vardas){
-    alert(Vardas);
-}
+// function callAlert(Vardas){
+//     alert(Vardas);
+// }
 
-function correctName(vardas, callBack){
-    let Vardas = vardas.charAt(0).toUpperCase() + vardas.slice(1);
-    callBack(Vardas);
-}
+// function correctName(vardas, callBack){
+//     let Vardas = vardas.charAt(0).toUpperCase() + vardas.slice(1);
+//     callBack(Vardas);
+// }
 
-// Example usage
-correctName('petras', consoleName);
-correctName('petras', callAlert);
+// // Example usage
+// correctName('petras', consoleName);
+// correctName('petras', callAlert);
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, ]
+
+// arr.forEach(num =>{
+//    console.log(num + 'skaicius')
+// })
+
+
+// console.log(arr)
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, ]
+
+// let newArr = arr.map(num =>{
+//   return(num + 10)
+// })
+
+
+// console.log(arr)
+// console.log(newArr)
+
+//------------------------------------------------
+
+// let notEven = (num)=>num % 2=== 0 
+
+
+// console.log(arr.every(notEven))

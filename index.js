782f4d2380063eaee45b60a0ef897c0cdd3c8cf2
akmindaugas <mindaugas.akelis@gmail.com>
2024-01-09 19:09:22 +0200

    commit_last_this_evening

--- index.js
+++ index.js
@@ -61,3 +61,81 @@
 
 
 // console.log(arr.every(notEven))
+
+// let arr = ['anuPras', 'zeniA', 'AlbynAs', 'kauspedas', 'Liolia']
+
+
+
+
+
+// const newArr = arr.map((v) => v.charAt(0).toUpperCase() + v.slice(1).toLowerCase());
+// console.log(newArr);
+
+
+// let metai = [2, 5, 4, 3, 8, 9, 1233]
+
+// let newMetai = metai.filter((m)=> m >= 25)
+// console.log(newMetai)
+
+// let miestai = ['klaipeda', 'kedainiai', 'alytus', 'radviliskis']
+// let newMiestai = miestai.find(str => str.charAt(0)==='k')
+// console.log(newMiestai)
+
+// let newMiestai = miestai.some(firstCapitalLetter)
+// function firstCapitalLetter(miestai){
+//     return str.charAt(0)=== firstCapitalLetter
+// }
+
+// const cities = ["Vilnius", "Kaunas", "klaipeda"];
+// console.log(cities.every(v => v.charAt(0) === v.charAt(0).toUpperCase()));
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+
+
+// let newArr = (arr, num) => {
+//     arr.push(num)
+
+//   return arr
+// }
+
+// console.log(newArr([1, 2, 3, 4], 78))
+
+// const addElement = (arr, element) => {
+//     arr.push(element);
+//     return arr;
+// };
+// console.log(addElement([1, 2, 3], 4)); // [1, 2, 3, 4]
+
+// let newArr = miestai.concat(arr)
+
+// console.log(newArr)
+
+
+// let newArr= (arr, ...elements) => [arr, ...elements]
+
+// console.log([arr], (4, 5, 6, 4, 8))
+
+
+
+// const addMultipleElements = (arr, ...elements) => [...arr, ...elements];
+
+// console.log(addMultipleElements([1, 2, 3], 4, 5, 6)); // [1, 2, 3, 4, 5, 6]
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+
+// let newArr = (arr)=> arr.map(num => num * 2)
+
+// console.log(newArr(arr))
+
+
+// const doubleNumbers = (arr) => arr.map(number => number * 2);
+// console.log(doubleNumbers([1, 2, 3])); // [2, 4, 6]
+
+
+
+let findUnique = (arr)=> 
+
+
+
+
+console.log(findUnique([1, 2, 2, 3, 4, 4, 5])); // [1, 2, 3, 4, 5]
